73fb6d547d9f168144429c9fe9035357e7079139
jito <jitokj3@gmail.com>
2021-03-09 22:32:48 +0530

    react native element and react navigation

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
+
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createStackNavigator();
+const globalScreenOptions = {
+  headerStyle = {backgroundColor: "#2c6bed"},
+  headerTitleStyle: {color:"white"},
+  headerTintColor: "white"
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>signal</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # signal
 
 React-Native App
+
+### React Navigation v5.0
+
+### React Native Elements
